424854b440a02c031f4b2f5cfcbc630a9291db14
winniekwokwy <206894518+winniekwokwy@users.noreply.github.com>
2025-05-26 23:09:18 +0100

    handle non-integer input and refactor the code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,52 +1,94 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.IO.Pipelines;
+using System.Runtime.CompilerServices;
 
+int getUserInput(string message)
+{
+    int result = 0;
+    do
+    {
+        Console.Write(message);
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out result))
+        {
+            return result;
+        }
+    } while (true);
+}
+
+int add(int[] numbers)
+{
+    int result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result += numbers[i];
+    }
+    return result;
+}
+
+int minus(int[] numbers)
+{
+    int result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result -= numbers[i];
+    }
+    return result;
+}
+
+int multiply(int[] numbers)
+{
+    int result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result *= numbers[i];
+    }
+    return result;
+}
+
+float divide(int[] numbers)
+{
+    float result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result /= numbers[i];
+    }
+    return result;
+}
 Console.WriteLine("Welcome to the calculator!");
 Console.WriteLine("==========================");
+
 Console.Write("Please enter the operator :");
 string oper = Console.ReadLine();
 
-Console.Write("How many numbers do you want to " + oper + " :");
-int noOfNo = int.Parse(Console.ReadLine());
+
+int noOfNo = getUserInput("How many numbers do you want to " + oper + " :");
 
 int[] noArray = new int[noOfNo];
 
 for (int i = 0; i < noOfNo; i++)
 {
-    Console.Write("Please enter number "+(i+1)+" :");
-    noArray[i]= int.Parse(Console.ReadLine());
+    noArray[i]= getUserInput("Please enter number "+(i+1)+" :");
 }
 
-float result = noArray[0];
+float result = 0;
 
 if (oper == "+")
 {
-    for (int i = 1; i < noOfNo; i++)
-    {
-        result += noArray[i];
-    }
-
+    result = add(noArray);
 }
 else if (oper == "-")
 {
-    for (int i = 1; i < noOfNo; i++)
-    {
-        result -= noArray[i];
-    }
+    result = minus(noArray);
 }
 else if (oper == "*")
 {
-    for (int i = 1; i < noOfNo; i++)
-    {
-        result *= noArray[i];
-    }
+    result = multiply(noArray);
 }
 else if (oper == "/")
 {
-    for (int i = 1; i < noOfNo; i++)
-    {
-        result /= noArray[i];
-    }
+    result = divide(noArray);
 }
 
 Console.WriteLine("The answer is " + result.ToString());
