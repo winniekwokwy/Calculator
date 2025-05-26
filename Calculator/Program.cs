ce4dba329cfd6438e367952da58dc8c6746a0b6f
winniekwokwy <winniekwokwy@yahoo.com>
2025-05-26 18:20:08 +0100

    add a simple multiplier

---
+++ Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the calculator!");
+Console.Write("Please tell me a number :");
+int firstNo = int.Parse(Console.ReadLine());
+
+Console.Write("Please tell me another number :");
+int secondNo = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The result of multiplying the two numbers is " + (firstNo * secondNo).ToString());
+Console.ReadLine();
